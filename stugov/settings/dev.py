acd38f68720208c6f9d608d283285edc3c227d44
rtsanders <rileytsanders@gmail.com>
2026-06-13 12:12:40 -0400

    Source SECRET_KEY from Env
    Edited dev.py and production.py to pull the SECRET_KEY from the environment variable DJANGO_SECRET_KEY, while dev.py falls back to a hardcoded insecure key.

--- stugov/settings/dev.py
+++ stugov/settings/dev.py
@@ -1,10 +1,11 @@
 from .base import *
+import os
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-m9(x9w!lhf-4sv7&7h^jci*m7c7+2vg0lhha4qrb71qzn5tp$j"
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "django-insecure-m9(x9w!lhf-4sv7&7h^jci*m7c7+2vg0lhha4qrb71qzn5tp$j")
 
 # SECURITY WARNING: define the correct hosts in production!
 ALLOWED_HOSTS = ["*"]
