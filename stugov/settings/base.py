6221208eafa00567b52ca27b5fb55dff2af145fd
Ju-Fi <72897587+Ju-Fi@users.noreply.github.com>
2026-08-02 13:13:42 -0400

    Fix incorrect argument order

--- stugov/settings/base.py
+++ stugov/settings/base.py
@@ -193,7 +193,7 @@ EMAIL_PORT = int(os.environ.get("EMAIL_PORT", 587))
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER", "")
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD", "")
-DEFAULT_FROM_EMAIL = os.environ.get("DEFAULT_FROM_EMAIL", "stugov_site@union.lists.rpi.edu")
+DEFAULT_FROM_EMAIL = os.environ.get("DEFAULT_FROM_EMAIL", "")
 
 # Wagtail settings
 
